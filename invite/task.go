d283819055cc8d61ae522a57763e42c36ebec374
rakin <rakin@rakins-MacBook-Pro.local>
2018-03-16 10:30:24 +0600

    inviter without applying filter

--- invite/task.go
+++ invite/task.go
@@ -1,18 +1,66 @@
 package invite
 
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+)
+
 type task struct {
 	fileLoc string
+
+	customers []Customer
 
 	err error
 }
 
+func (t task) hasErr() bool {
+	if t.err != nil {
+		return true
+	}
+
+	return false
+}
+
+func (t *task) clear() {
+	t.err = nil
+	t.customers = []Customer{}
+}
+
+func (t *task) append(str string) {
+	if t.hasErr() {
+		return
+	}
+
+	cust := Customer{}
+	err := json.Unmarshal([]byte(str), &cust)
+	if err != nil {
+		t.err = err
+		return
+	}
+
+	t.customers = append(t.customers, cust)
+}
+
 func (t *task) Read(fileLoc string) {
+	t.clear()
+
+	f, err := os.Open(fileLoc)
+	if err != nil {
+		t.err = err
+		return
+	}
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		t.append(s.Text())
+	}
 
 }
 
 func (t *task) List(f Filter) []Customer {
 
-	return []Customer{}
+	return t.customers
 }
 
 func (t task) Err() error {
